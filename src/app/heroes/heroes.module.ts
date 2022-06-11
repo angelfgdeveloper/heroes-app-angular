fbe01b66403f32c408fe94a20ace7886c4da3c45
Luis Angel <angelfgdeveloper@gmail.com>
2022-06-11 15:01:03 -0500

    Primera seccion terminada

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { HeroesRountingModule } from './heroes-rounting.module';
+import { MaterialModule } from '../material/material.module';
+import { FormsModule } from '@angular/forms';
+
+import { AgregarComponent } from './pages/agregar/agregar.component';
+import { BuscarComponent } from './pages/buscar/buscar.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ListadoComponent } from './pages/listado/listado.component';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
+
+// Pipes
+import { ImagenPipe } from './pipes/imagen.pipe';
+
+@NgModule({
+  declarations: [
+    AgregarComponent,
+    BuscarComponent,
+    HeroeComponent,
+    HomeComponent,
+    ListadoComponent,
+    HeroeTarjetaComponent,
+
+    // Pipes
+    ImagenPipe,
+  ],
+  imports: [
+    CommonModule,
+    FlexLayoutModule,
+    FormsModule,
+    MaterialModule,
+    HeroesRountingModule
+  ]
+})
+export class HeroesModule { }
